9a66ca9067572907a6c6aff72d63c43a2792396f
l3lackcurtains <9688803+l3lackcurtains@users.noreply.github.com>
2019-07-26 17:11:36 +0545

    expense categorization

--- src/graphql/expense/get.resolver.ts
+++ src/graphql/expense/get.resolver.ts
@@ -1,21 +1,23 @@
 import { GraphQLError } from "graphql";
+import { getRepository } from "typeorm";
 import { Expense } from "../../entity/Expense";
 
 export default {
   Query: {
     getExpenses: async (_: any, args: any, ctx: any) => {
       try {
-        const expenses = await Expense.find({
-          cache: true,
-          relations: ["user", "project"]
-        });
+        const expenses = await getRepository(Expense)
+          .createQueryBuilder("expense")
+          .orderBy("expense.date", "DESC")
+          .innerJoinAndSelect("expense.user", "user")
+          .innerJoinAndSelect("expense.project", "project")
+          .getMany();
 
         if (!expenses) {
           throw new GraphQLError("Expenses not found.");
         }
         return expenses;
       } catch (e) {
-        console.log(e);
         if (e instanceof GraphQLError) throw e;
         throw new Error("Something went wrong.");
       }
@@ -36,6 +38,48 @@ export default {
         if (e instanceof GraphQLError) throw e;
         throw new Error("Something went wrong.");
       }
+    },
+    getGroupedExpensesStat: async (_: any, args: any, ctx: any) => {
+      const expenses = await getRepository(Expense)
+        .createQueryBuilder("expense")
+        .getMany();
+
+      return getGroupedExpenseCategories(expenses);
     }
   }
+};
+
+const getGroupedExpenseCategories = (expenses: Expense[]) => {
+  const groupedCategory: any = {};
+  const categories: string[] = [];
+  expenses.forEach((expense: Expense) => {
+    if (categories.includes(expense.category)) {
+      groupedCategory[expense.category] += expense.amount;
+    } else {
+      categories.push(expense.category);
+      groupedCategory[expense.category] = expense.amount;
+    }
+  });
+
+  const total = Object.keys(groupedCategory).reduce(
+    (sum: number, key: string) => sum + groupedCategory[key],
+    0
+  );
+
+  const categoryStat: any = [];
+
+  Object.keys(groupedCategory).forEach((category: string) => {
+    categoryStat.push({
+      category,
+      total: groupedCategory[category],
+      percentage: Math.round((groupedCategory[category] / total) * 100)
+    });
+  });
+
+  const result: any = {
+    total,
+    categoryStat
+  };
+
+  return result;
 };
